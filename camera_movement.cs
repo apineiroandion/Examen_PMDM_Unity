32ef4351eeade792165cfcabcf42a6328b82153f
apineiroandion <apineiroandion@danielcastelao.org>
2025-03-18 14:12:12 +0100

    adapto camara

--- camera_movement.cs
+++ camera_movement.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class camera_movement : MonoBehaviour
 {
     public Transform target; // La bola
-    public Vector3 offset = new Vector3(0f, 5f, -7f); // Distancia de la cámara a la bola
+    public Vector3 offset = new Vector3(10f, 5f, -7f); // Distancia de la cámara a la bola
+
 
     void LateUpdate()
     {
         if (target != null)
         {
-            transform.position = target.position + offset; // Sigue la bola manteniendo la distancia
-            transform.LookAt(target); // Hace que la cámara mire a la bola
+            // Mantener la cámara en la misma posición relativa a la bola, pero en diagonal
+            transform.position = target.position + offset;
+
+            // La cámara siempre mira a la bola
+            transform.LookAt(target);
         }
     }
 }
+
